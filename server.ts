e504986051f18b92ef6055c092e7f4a8c2a82ab3
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-07-22 21:25:04 +0200

    init

--- server.ts
+++ server.ts
@@ -2,7 +2,18 @@ import express from 'express'
 const app=express()
 
 app.get('/post',(req,res)=>{
-    res.send('all posts')
+    res.send('one post ')
+})
+app.get('/posts',(req,res)=>{
+    res.json({posts:[{
+        id:'abc',
+        title:'post one title',
+        content:'post one content'
+    },{
+        id:'ark',
+        title:'post two title',
+        content:'post two content'
+    }]})
 })
 
 app.listen(3000,()=>console.log('server is running at port 3000'))
